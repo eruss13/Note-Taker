01ca7332c4838894fd51f069dd339afcca0aca11
Ethan Russell <63941167+eruss13@users.noreply.github.com>
2021-02-19 01:46:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+require('./routes/routes')(app);
+
+
+app.listen(PORT, function() {
+    console.log(`Now listening to port ${PORT}`);
+})
